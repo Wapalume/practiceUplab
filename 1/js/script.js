e7741dc8a3ea7efc52dea416462412bb1c2c82c3
Wapalume <wapalume@gmail.com>
2022-05-12 13:45:24 +0300

    practice

---
+++ 1/js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+//БУРГЕР
+const iconMenu = document.querySelector(".header__burger");
+const menuBody = document.querySelector(".header__body");
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
+
+function ibg() {
+  let ibg = document.querySelectorAll("._ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
+}
+ibg();
+
+// прокрутка при клике
+
+const menuLinks = document.querySelectorAll(".header__link[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        window.pageYOffset -
+        document.querySelector("header").offsetHeight;
+
+      if (iconMenu.classList.contains("_active")) {
+        document.body.classList.remove("_lock");
+        iconMenu.classList.remove("_active");
+        menuBody.classList.remove("_active");
+      }
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      e.preventDefault();
+    }
+  }
+}
